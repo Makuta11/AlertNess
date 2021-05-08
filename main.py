a2eb1cf7d5b3e336ffefee9ccb4a072071248dbe
Christopher Ravn Boe Jensen <DG@Christophers-MacBook-Pro-5.local>
2021-05-08 13:43:27 +0200

    epoch

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ kf = KFold(n_splits = 4, shuffle=True, random_state=42)
 numSplits = kf.get_n_splits(X)
 
 # Training Parameters
-epochs = 50
+epochs = 1
 batch_size = 32 
 #callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
 
